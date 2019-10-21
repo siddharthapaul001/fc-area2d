fd8ec883dcfdaf1045d684b103e09569bb442a4b
Siddhartha Paul <siddharthapaul@live.com>
2019-10-21 19:06:15 +0530

    Annotations initiated

--- js/test-specs-annotations.js
+++ js/test-specs-annotations.js
@@ -1,4 +1,23 @@
 tests = [
     ...tests,
-    
+    {
+        id: "test-annotation-1",
+        description: "Implementing custom star path inside fusioncharts",
+        test: {
+            dataSource: {
+                chart: {
+                    caption: "Test Annotation",
+                    subCaption: "Test Annotation sample"
+                },
+                data: dobData
+            },
+            annotations: [{
+                groups: [{
+                    items: [{
+                        
+                    }]
+                }]
+            }]
+        }
+    }
 ]
